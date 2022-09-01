c399677c6cc32153ad0d92cb5fb6ce880fde069d
frame88 <fleuzzi97@gmail.com>
2022-09-01 16:19:19 +0200

    esercizio simile al precedente ma con la creazione di classi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+// Versione 1
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <li *ngFor="let user of users"     
-     [class.male]="user.gender === 'M'"
-     [class.female]="user.gender === 'F'"
-    >
-    {{user.name}}
-  </li>
-       
+    <li
+      *ngFor="let user of users"
+      [ngClass]="{
+        'male': user.gender === 'M',
+        'female': user.gender === 'F'
+      }"
+    >{{user.name}}</li>
   `,
   styles: [`
-    .male { color: blue }
-    .female { color: pink }
+    .male { background-color: blue; color: white}
+    .female { background-color: pink }
   `]
 })
 export class AppComponent {
@@ -24,28 +25,3 @@ export class AppComponent {
     { id: 4, name: 'Silvia', gender: 'F' }
   ];
 }
-
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   template: `
-//     <li
-//       *ngFor="let user of users"
-//       [class.male]="user.gender === 'M'"
-//       [class.female]="user.gender === 'F'"
-//     >{{user.name}}</li>
-//   `,
-//   styles: [`
-//     .male { background-color: blue, color: white }
-//     .female { background-color: pink }
-//   `]
-// })
-// export class AppComponent {
-//   users = [
-//     { id: 1, name: 'Fabio', gender: 'M' },
-//     { id: 2, name: 'Lisa', gender: 'F' },
-//     { id: 3, name: 'Lorenzo', gender: 'M' },
-//     { id: 4, name: 'Silvia', gender: 'F' }
-//   ];
-// }

967dbd4973a7b3f2b4dafaa7250be30df495aaca
frame88 <fleuzzi97@gmail.com>
2022-09-01 14:56:44 +0200

    lista di nomi con pulsante delete che cancella i nomi dalla lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <li *ngFor="let nome of listanomi; let i = index; let dispari = odd;">
-      <h1>{{i}} {{nome}}</h1>
-      <hr *ngIf="dispari">
-    </li>
+    <div class="container-fluid d-flex justify-content-center">
+      <div class="container">
+        <li *ngFor="let nome of listanomi; let i = index; let dispari = odd;">
+          <span class="fs-3 m-3">{{i}} {{nome}}</span>
+          <button class="bg-danger text-light" (click)="remove(nome)">remove</button>
+        </li>
+      </div>
+    </div>
   `,
   styles: [`li{list-style-type: none;}`]
 })
@@ -15,4 +19,10 @@ export class AppComponent {
 
 
   listanomi = ['piero', 'franco', 'mario', 'marco','lucio'];
+
+  remove(nome: string) {
+    console.log('remove work\n');
+    const index = this.listanomi.indexOf(nome);
+    this.listanomi.splice(index, 1);
+  }
 }

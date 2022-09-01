0f896e186142f45b19768f9a5e9a03eac60e9a73
frame88 <fleuzzi97@gmail.com>
2022-09-01 15:29:29 +0200

    inline styles rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container text-center mt-5">
-      <button class="m-5 p-3 rounded" (click)="goto('one')">one</button>
-      <button class="m-5 p-3 rounded" (click)="goto('two')">two</button>
-      <button class="m-5 p-3 rounded" (click)="goto('three')">three</button>
-  
-      <div [ngSwitch]="section">
-        <div *ngSwitchCase="'one'">
-          sce...
-        </div>
-        <div *ngSwitchCase="'two'">
-          ...mochi...
-        </div>
-        <div *ngSwitchDefault>
-          ...legge
-        </div>
-      </div>
-
-    </div>
-  `
+    <h1 class="male">hello world</h1>
+  `,
+  styles: [`
+    .male { color: blue }
+    .female { color: pink }
+  `]
 })
 export class AppComponent {
-  section = 'one';
 
-  goto(value: string) {
-    this.section = value;
-  }
 }

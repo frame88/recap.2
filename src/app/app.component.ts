cf845398c7baf3fed5c1ce2cda5e0dfca2731f6e
frame88 <fleuzzi97@gmail.com>
2022-09-01 15:26:32 +0200

    elementi differenti visualizzati a seconda del click (manipolazione del dom)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container-fluid d-flex justify-content-center">
-      <div class="container">
-        <li *ngFor="let nome of listanomi; let i = index; let dispari = odd;">
-          <span class="fs-3 m-3">{{i}} {{nome}}</span>
-          <button class="bg-danger text-light" (click)="remove(nome)">remove</button>
-        </li>
+    <div class="container text-center mt-5">
+      <button class="m-5 p-3 rounded" (click)="goto('one')">one</button>
+      <button class="m-5 p-3 rounded" (click)="goto('two')">two</button>
+      <button class="m-5 p-3 rounded" (click)="goto('three')">three</button>
+  
+      <div [ngSwitch]="section">
+        <div *ngSwitchCase="'one'">
+          sce...
+        </div>
+        <div *ngSwitchCase="'two'">
+          ...mochi...
+        </div>
+        <div *ngSwitchDefault>
+          ...legge
+        </div>
       </div>
+
     </div>
-  `,
-  styles: [`li{list-style-type: none;}`]
+  `
 })
 export class AppComponent {
-  title = 'recap.2';
+  section = 'one';
 
-
-  listanomi = ['piero', 'franco', 'mario', 'marco','lucio'];
-
-  remove(nome: string) {
-    console.log('remove work\n');
-    const index = this.listanomi.indexOf(nome);
-    this.listanomi.splice(index, 1);
+  goto(value: string) {
+    this.section = value;
   }
 }

8f9a8384f53d9ea6d167627f10597e5a6514362b
frame88 <fleuzzi97@gmail.com>
2022-09-02 12:40:19 +0200

    input, con aggiunta elementi ad una lista tramite un <button>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,45 @@
-import { Component } from '@angular/core';
- 
+// Input, "template reference variables", keyboard events
+import { Component, OnInit } from '@angular/core';
+
 @Component({
-   template: `
-   <li
-     *ngFor="let user of users"
-     [ngStyle]="getStyle(user)"
-   >{{user.name}}</li>
- `
+  selector: 'app-root',
+  template: `
+    <div class="container-fluid mt-5 pt-5">
+      <div class="container text-center">
+        <input
+          type="text"
+          #inputName
+          placeholder="Inserisci qualcosa"
+          (keyup.enter)="addUser(inputName)"
+        >
+        <button (click)="addUser(inputName)">Save</button>
+        <!-- <button (click)="remove(user)" class="bg-danger text-light">Remove</button> -->
+        
+        <li *ngFor="let user of users">{{user}}</li>
+      </div>
+    </div>
+  `,
+  styles: []
 })
 export class AppComponent {
-   users: User[] = [
-       { id: 1, name: 'Fabio', age: 30, gender: 'M' },
-       { id: 2, name: 'Lisa', age: 20, gender: 'F' },
-       { id: 3, name: 'Lorenzo', age: 10, gender: 'M' },
-       { id: 4, name: 'Silvia', age: 130, gender: 'F' }
-   ];
- 
-   getStyle(user:User) {
-       return {
-           backgroundColor: user.gender === 'M' ? 'black' : 'grey',
-           color: user.gender === 'M' ? 'blue' : 'pink',
-           fontSize: `${user.age}px`
-       };
-   }
-}
- 
-interface User {
-   id: number,
-   name: string,
-   gender: string,
-   age: number,
+  users: string[] = [];
+
+  addUser(input: HTMLInputElement) {
+    this.users.push(input.value);
+    input.value = '';
+    input.focus();
+  }
+
+  // remove(user: string) {
+  //   console.log('remove work\n');
+  //   const index = this.users.indexOf(user);
+  //   this.users.splice(index, 1);
+  // }
+
+
+  //Tentativo di creare segeuentemente al button Save, uno che faccia l'opposto.
+  // Come nell'esercizio qui eseguito: https://github.com/frame88/recap.2/commit/967dbd4973a7b3f2b4dafaa7250be30df495aaca
+  // l'ipotesi è che non riesca a rimuovere nulla poichè a differenza del codice proprio, qui abbiamo una lista (users) in continua
+  // espansione.
+  // Quindi il numero di iterazioni del for viene continuamente aggiornato, e il remove non sa dove parare (?)
 }

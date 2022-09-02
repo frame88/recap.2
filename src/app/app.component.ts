0dbf87778a8f319fc328517196c36333eec2cb32
frame88 <fleuzzi97@gmail.com>
2022-09-02 13:59:19 +0200

    lista di nomi che vengono dati in input, mentre l'input viene copiato in tempo reale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,32 @@
-// Input, "template reference variables", keyboard events
-import { Component, OnInit } from '@angular/core';
+// La direttiva ngModel: 1way vs 2 way binding
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container-fluid mt-5 pt-5">
-      <div class="container text-center">
-        <input
-          type="text"
-          #inputName
-          placeholder="Inserisci qualcosa"
-          (keyup.enter)="addUser(inputName)"
-        >
-        <button (click)="addUser(inputName)">Save</button>
-        <!-- <button (click)="remove(user)" class="bg-danger text-light">Remove</button> -->
-        
-        <li *ngFor="let user of users">{{user}}</li>
-      </div>
-    </div>
-  `,
-  styles: []
+    <input
+      type="text"
+      placeholder="Add User name"
+      [(ngModel)]="label"
+    >
+    <button (click)="add()">ADD</button>
+    <span>{{label}}</span>
+    <li *ngFor="let user of users">{{user}}</li>
+  `
 })
 export class AppComponent {
+  label: string = 'guest';
   users: string[] = [];
 
-  addUser(input: HTMLInputElement) {
-    this.users.push(input.value);
-    input.value = '';
-    input.focus();
+  add() {
+    console.log(this.label);
+    // Add to list
+    this.users.push(this.label);
+    this.label = '';
   }
-
-  // remove(user: string) {
-  //   console.log('remove work\n');
-  //   const index = this.users.indexOf(user);
-  //   this.users.splice(index, 1);
-  // }
-
-
-  //Tentativo di creare segeuentemente al button Save, uno che faccia l'opposto.
-  // Come nell'esercizio qui eseguito: https://github.com/frame88/recap.2/commit/967dbd4973a7b3f2b4dafaa7250be30df495aaca
-  // l'ipotesi è che non riesca a rimuovere nulla poichè a differenza del codice proprio, qui abbiamo una lista (users) in continua
-  // espansione.
-  // Quindi il numero di iterazioni del for viene continuamente aggiornato, e il remove non sa dove parare (?)
 }
+
+
+
+// modifica all'esercizio con:
+// < span > {{ label }}</>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+// import { FormsModule } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
